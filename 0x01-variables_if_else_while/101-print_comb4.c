da867fc5f16dc9883c143259a2f091583e90abe3
Vwairhe1405 <vwairheokolosi@gmail.com>
2022-07-02 05:24:34 -0700

    101 print

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -2,8 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- * main - Prints all possible combinations of three different digits,
- * 	in ascending order, separated by a comma followed by a space
+ * main - main function
  *
  * Return: Always 0
  */
