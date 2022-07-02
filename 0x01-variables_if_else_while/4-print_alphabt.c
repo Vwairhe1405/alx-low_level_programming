b2bc99bf4a690aa18c909e3f9784f8ef58dfced7
Vwairhe1405 <vwairheokolosi@gmail.com>
2022-07-02 01:34:21 -0700

    Alphabet 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Print the alphabet in lowercase, except for q and e
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter != 'e' && letter != 'q')
+			putchar(letter);
+	}
+	putchar('\n');
+	return (0);
+}
