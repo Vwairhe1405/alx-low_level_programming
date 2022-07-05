e7d1015afb2ba352f93204feeeef9ca7a00560ae
Vwairhe1405 <vwairheokolosi@gmail.com>
2022-07-05 13:51:29 -0700

    isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * _isalpha - Checks if the character is alphabetic.
- * @c: The alphabet to be checked
+ * @c: The character to be checked
  *
  * Return 1 if c is lowercase or uppercase, 0 otherwise.
  */
