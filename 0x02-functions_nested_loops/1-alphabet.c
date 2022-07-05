ff927675cf10010c9b2cbdf19613ced9fb75abd7
Vwairhe1405 <vwairheokolosi@gmail.com>
2022-07-05 12:24:59 -0700

    Alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,13 +4,11 @@
  *
  * Return: Always 0
  */
-int main(void)
+void print_alphabet(void)
 {
 	char letter;
 
 	for (letter = 'a'; letter <= 'z'; letter++)
 		_putchar(letter);
 	_putchar('\n');
-
-	return (0);
 }
