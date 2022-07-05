ae44270d1265add4e3f2a628ed42654910dd6b1e
Vwairhe1405 <vwairheokolosi@gmail.com>
2022-07-05 11:56:14 -0700

    Alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - main function
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+	_putchar('\n');
+
+	return (0);
+}
