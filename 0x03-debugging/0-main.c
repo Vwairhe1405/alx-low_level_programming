0db99da256ee479b2dc03686c26a4efaab76f665
Vwairhe1405 <vwairheokolosi@gmail.com>
2022-07-07 02:44:29 -0700

    Multiple main

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,25 +1,15 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include "main.h"
 
 /**
- *
  * main - tests function that prints if integer is positive or negative
- *
- * Return: Always 0
+ * Return: 0
  */
-
 int main(void)
 {
-	int n;
-	
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	if (n > 0)
-		printf("%d is positive\n", n);
-	else if (n == 0)
-		printf("%d is zero\n", n);
-	else
-		printf("%d is negative\n", n);
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
 	return (0);
 }
---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - avoid an infinite loop
+ *
+ * Return: 1
+ */
+int main(void)
+{
+	int i;
+	
+	printf("infinite loop incoming :('\n');
+			i = 0;
+
+	while (i < 10)
+	{
+		putcahr(1);
+	}
+
+	printf("Infinite loop avoided! \\o/\n");
+
+	return (0);
+}
