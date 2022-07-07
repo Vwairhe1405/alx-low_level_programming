7822516d4b59fcfc4e3f26c62855654579639d6b
Vwairhe1405 <vwairheokolosi@gmail.com>
2022-07-07 12:10:49 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -10,8 +10,6 @@ void print_numbers(void)
 	int num;
 
 	for (num = 0; num <= 9; num++)
-		_putchar((num % 10) + '0');
-	_putchar('\n');
-
-	return (0);
+		putchar((num % 10) + '0');
+	putchar('\n');
 }
