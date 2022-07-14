02ca39acec308be6f8ddd4ea8c55f6bebf7d49b3
Vwairhe1405 <vwairheokolosi@gmail.com>
2022-07-14 01:39:03 -0700

    Strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcmp - function that compares two string
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: Return integer
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int result;
+
+	result = strcmp(s1, s2);
+
+	return (result);
+}
