c39da17696f7824e274036ddad0fd8f4c6675bc0
Vwairhe1405 <vwairheokolosi@gmail.com>
2022-07-14 04:24:57 -0700

    Leet

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -2,28 +2,28 @@
 
 /**
  * leet - function that encodes a string into 1337.
- * @y: The string to be encoded
+ * @x: The string to be encoded
  *
  * Return: Return pointer to encoded string
  *
  */
-char *leet(char *y)
+char *leet(char *x)
 {
 	int a = 0, b, l = 5;
 	char tr[5] = {'A', 'E', 'O', 'T', 'L'};
 	char trw[5] = {'4', '3', '0', '7', '1'};
 
-	while (y[a])
+	while (x[a])
 	{
 		b = 0;
 
 		while (b < 1)
 		{
-			if (y[a] == tr[b] || y[a] - 32 == tr[b])
-				y[a] = trw[b];
+			if (x[a] == tr[b] || x[a] - 32 == tr[b])
+				x[a] = trw[b];
 			b++;
 		}
 		a++;
 	}
-	return (y);
+	return (x);
 }
