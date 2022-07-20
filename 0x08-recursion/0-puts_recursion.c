2a37e056c43b09c4f9b144fd4a93c75cd2ae390c
Vwairhe1405 <vwairheokolosi@gmail.com>
2022-07-20 03:52:00 -0700

    Task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -10,12 +10,12 @@
  */
 void _puts_recursion(char *s)
 {
-	if (*s)
+	if (*s == '\0')
 	{
-		_putchar(*s);
-		_puts_recursion(s + 1);
+		putchar('\n');
+		return;
 	}
-		else
-	_putchar('\n');
-
+	putchar(*s);
+	s++;
+	_puts_recursion(s);
 }
