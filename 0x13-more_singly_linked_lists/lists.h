cf1ca6c4b36dd61dfeeba2cae4d53c9bebb27fc5
Vwairhe1405 <vwairheokolosi@gmail.com>
2022-08-16 02:47:17 -0700

    Add node at the end

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -17,5 +17,7 @@ listint_t *createNode(const int prmNbr);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
+listint_t *add_nodeint_end(listint_t **head, const int n);
+listint_t *getLastNode(listint_t *prmHeadNode);
 
 #endif
