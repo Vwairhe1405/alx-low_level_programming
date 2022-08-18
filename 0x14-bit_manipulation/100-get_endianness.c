db64ce8f01f4c323d9f098723587253b9c5ad2b6
Vwairhe1405 <vwairheokolosi@gmail.com>
2022-08-18 14:52:24 -0700

    Endianness

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * get_endianness - checks the endianness
+ *
+ * Return: true if check
+ */
+int get_endianness(void)
+{
+	unsigned int x = 1;
+
+	char *c = (char *) &x;
+
+	return ((int) *c);
+}
